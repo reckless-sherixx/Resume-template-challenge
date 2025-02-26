49f91ab85cf98879e85672ccb3f95a41c0c9ec13
Vidyansh <ezboi2312@gmail.com>
2025-02-26 14:06:44 +0530

    build time errors

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,10 +5,8 @@ const About = ({ data }) => {
     var name = data.name;
     var profilepic = "images/" + data.image;
     var bio = data.bio;
-    var street = data.address.street;
     var city = data.address.city;
     var state = data.address.state;
-    var zip = data.address.zip;
     var phone = data.phone;
     var email = data.email;
     var resumeDownload = data.resumedownload;
