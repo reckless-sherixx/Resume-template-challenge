e31b518796385b38a1ee77b98b807567593f1461
Vidyansh <ezboi2312@gmail.com>
2025-02-26 16:48:04 +0530

    minor changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,6 @@ const Header = ({ data }) => {
     var name = data.name;
     var occupation = data.occupation;
     var description = data.description;
-    var city = data.address.city;
     var networks = data.social.map(function (network) {
       return (
         <li key={network.name}>
@@ -63,7 +62,7 @@ const Header = ({ data }) => {
             <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
           </h1>
           <h3>
-            Based in {city}. <span>{occupation}</span>. {description}.
+            Based in India. <span>{occupation}</span>. {description}.
           </h3>
           <hr />
           <ul className="social">{networks}</ul>
